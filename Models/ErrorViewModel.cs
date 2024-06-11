247903d3964bbbb67f40be1367aed93b5e3597f6
Skylimsk <skylimsk@hotmail.com>
2024-06-11 15:36:46 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DHL_ecommerce_login_page.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

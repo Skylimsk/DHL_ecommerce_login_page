ed7af8c95e16132588c48c150f6dc09ccab333c8
Skylimsk <skylimsk@hotmail.com>
2024-06-12 16:55:26 +0800

    Update HomePage Design

--- obj/Debug/net8.0/DHL_ecommerce_login_page.AssemblyInfo.cs
+++ obj/Debug/net8.0/DHL_ecommerce_login_page.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DHL_ecommerce_login_page")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f1ee2c9e84fbc0ec1cc6b14750031c987f3f811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d77988a568f4d02e56bd98628340a476698f3be")]
 [assembly: System.Reflection.AssemblyProductAttribute("DHL_ecommerce_login_page")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DHL_ecommerce_login_page")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
